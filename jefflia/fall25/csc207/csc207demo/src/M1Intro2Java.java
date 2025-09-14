c973e47c73bf7a4a780a547ae12a5f62a5a7ede6
Jeff Liang <jefflyf2006@163.com>
2025-09-13 23:01:13 -0400

    csc207note update 9/13

---
+++ jefflia/fall25/csc207/csc207demo/src/M1Intro2Java.java
@@ -0,0 +1,12 @@
+public class M1Intro2Java {
+    public static void main(String[] args) {
+        // 这是python的起始方法，运行此程序意味着运行这个方法内的内容
+    }
+
+    public void DefMethod(){
+        // 等价于Python  print(5+7)
+        System.out.println(5 + 7);
+    }
+
+    
+}
